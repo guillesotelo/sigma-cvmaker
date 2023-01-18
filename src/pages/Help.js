4a549a6770fb8da85481140578c76fb4c7f627d2
Guillermo Sotelo <guillermo.sotelo@conexiogroup.com>
2023-01-18 19:47:05 +0100

    finished search bar for help pages

--- src/pages/Help.js
+++ src/pages/Help.js
@@ -10,16 +10,21 @@ import { toast } from 'react-toastify'
 import { APP_COLORS } from '../constants/app'
 import SearchBar from '../components/SearchBar'
 import PageIcon from '../icons/page-icon.svg'
+import { HELP } from '../constants/help'
 
 export default function Help() {
     const [data, setData] = useState({})
     const [isEdit, setIsEdit] = useState(false)
     const [loading, setLoading] = useState(false)
     const [search, setSearch] = useState([])
-    const [modules, setModules] = useState([])
+    const [modules, setModules] = useState(HELP ? JSON.stringify(HELP) : '')
     const user = localStorage.getItem('user') && JSON.parse(localStorage.getItem('user'))
     const history = useHistory()
     const dispatch = useDispatch()
+
+    useEffect(() => {
+        if (!search.length) setModules(HELP ? JSON.stringify(HELP) : '')
+    }, [search])
 
     const updateData = (key, value) => {
         setIsEdit(true)
@@ -35,18 +40,21 @@ export default function Help() {
         }
     }
 
+    // console.log("modules", modules)
+
     const triggerSearch = () => {
         setLoading(true)
         if (search.length) {
-            const filtered = modules.filter(module => {
-                const stringLog = JSON.stringify(module)
+            const keys = Object.keys(HELP)
+            const filtered = keys.map(key => {
                 let matches = true
+                const stringModule = JSON.stringify(HELP[key])
                 search.forEach(word => {
-                    if (!stringLog.toLowerCase().includes(word.toLowerCase())) matches = false
+                    if (!stringModule.toLowerCase().includes(word.toLowerCase())) matches = false
                 })
-                if (matches) return module
+                if (matches) return key
             })
-            setModules(filtered)
+            setModules(filtered.join(''))
         }
         setLoading(false)
     }
@@ -67,88 +75,88 @@ export default function Help() {
             </div>
             <div className='help-module-row'>
                 <div className='help-module-col'>
-                    <div className='help-module-section'>
+                    {modules.includes('new-cv') || modules.includes('edit-cv') || modules.includes('restore-items') ? <div className='help-module-section'>
                         <h4 className='help-module-title'>Getting Started</h4>
-                        <div className='help-module-page'>
+                        {modules.includes('new-cv') ? <div className='help-module-page'>
                             <img src={PageIcon} className='help-page-svg' />
                             <h4 className='help-module-link' onClick={() => history.push(`/helpPage?module=new-cv`)}>Creating a new CV</h4>
-                        </div>
-                        <div className='help-module-page'>
+                        </div> : ''}
+                        {modules.includes('edit-cv') ? <div className='help-module-page'>
                             <img src={PageIcon} className='help-page-svg' />
                             <h4 className='help-module-link' onClick={() => history.push(`/helpPage?module=edit-cv`)}>Editing a CV</h4>
-                        </div>
-                        <div className='help-module-page'>
+                        </div> : ''}
+                        {modules.includes('restore-items') ? <div className='help-module-page'>
                             <img src={PageIcon} className='help-page-svg' />
                             <h4 className='help-module-link' onClick={() => history.push(`/helpPage?module=restore-items`)}>Restore a deleted item</h4>
-                        </div>
-                    </div>
-                    <div className='help-module-section'>
+                        </div> : ''}
+                    </div> : ''}
+                    {modules.includes('type-of-data') || modules.includes('activity-and-statistics') ? <div className='help-module-section'>
                         <h4 className='help-module-title'>App Data Management</h4>
-                        <div className='help-module-page'>
+                        {modules.includes('type-of-data') ? <div className='help-module-page'>
                             <img src={PageIcon} className='help-page-svg' />
                             <h4 className='help-module-link' onClick={() => history.push(`/helpPage?module=type-of-data`)}>Types of data</h4>
-                        </div>
-                        <div className='help-module-page'>
+                        </div> : ''}
+                        {modules.includes('activity-and-statistics') ? <div className='help-module-page'>
                             <img src={PageIcon} className='help-page-svg' />
                             <h4 className='help-module-link' onClick={() => history.push(`/helpPage?module=activity-and-statistics`)}>Activity & Statistics</h4>
-                        </div>
-                    </div>
+                        </div> : ''}
+                    </div> : ''}
                 </div>
                 <div className='help-module-col'>
-                    <div className='help-module-section'>
+                    {modules.includes('new-image') || modules.includes('image-styling') || modules.includes('tips-profile') ? <div className='help-module-section'>
                         <h4 className='help-module-title'>Images Module</h4>
-                        <div className='help-module-page'>
+                        {modules.includes('new-image') ? <div className='help-module-page'>
                             <img src={PageIcon} className='help-page-svg' />
                             <h4 className='help-module-link' onClick={() => history.push(`/helpPage?module=new-image`)}>Upload a new image</h4>
-                        </div>
-                        <div className='help-module-page'>
+                        </div> : ''}
+                        {modules.includes('image-styling') ? <div className='help-module-page'>
                             <img src={PageIcon} className='help-page-svg' />
                             <h4 className='help-module-link' onClick={() => history.push(`/helpPage?module=image-styling`)}>Edit image styling</h4>
-                        </div>
-                        <div className='help-module-page'>
+                        </div> : ''}
+                        {modules.includes('tips-profile') ? <div className='help-module-page'>
                             <img src={PageIcon} className='help-page-svg' />
                             <h4 className='help-module-link' onClick={() => history.push(`/helpPage?module=tips-profile`)}>Tips for a nice profile image</h4>
-                        </div>
-                    </div>
-                    <div className='help-module-section'>
+                        </div> : ''}
+                    </div> : ''}
+                    {modules.includes('common-errors') || modules.includes('submit-report') ? <div className='help-module-section'>
                         <h4 className='help-module-title'>Troubleshooting</h4>
-                        <div className='help-module-page'>
+                        {modules.includes('common-errors') ? <div className='help-module-page'>
                             <img src={PageIcon} className='help-page-svg' />
                             <h4 className='help-module-link' onClick={() => history.push(`/helpPage?module=common-errors`)}>Common errors</h4>
-                        </div>
-                        <div className='help-module-page'>
+                        </div> : ''}
+                        {modules.includes('sumbit-report') ? <div className='help-module-page'>
                             <img src={PageIcon} className='help-page-svg' />
                             <h4 className='help-module-link' onClick={() => history.push(`/helpPage?module=sumbit-report`)}>Submit a report</h4>
-                        </div>
-                    </div>
+                        </div> : ''}
+                    </div> : ''}
                 </div>
                 <div className='help-module-col'>
-                    <div className='help-module-section'>
+                    {modules.includes('new-account') || modules.includes('edit-user') || modules.includes('what-to-know-consultant-data') ? <div className='help-module-section'>
                         <h4 className='help-module-title'>Users Module</h4>
-                        <div className='help-module-page'>
+                        {modules.includes('new-account') ? <div className='help-module-page'>
                             <img src={PageIcon} className='help-page-svg' />
                             <h4 className='help-module-link' onClick={() => history.push(`/helpPage?module=new-account`)}>Create a new account (Manager or Consultant)</h4>
-                        </div>
-                        <div className='help-module-page'>
+                        </div> : ''}
+                        {modules.includes('edit-user') ? <div className='help-module-page'>
                             <img src={PageIcon} className='help-page-svg' />
                             <h4 className='help-module-link' onClick={() => history.push(`/helpPage?module=edit-user`)}>Edit user data</h4>
-                        </div>
-                        <div className='help-module-page'>
+                        </div> : ''}
+                        {modules.includes('what-to-know-consultant-data') ? <div className='help-module-page'>
                             <img src={PageIcon} className='help-page-svg' />
                             <h4 className='help-module-link' onClick={() => history.push(`/helpPage?module=what-to-know-consultant-data`)}>What to know when updating consultant data</h4>
-                        </div>
-                    </div>
-                    <div className='help-module-section'>
+                        </div> : ''}
+                    </div> : ''}
+                    {modules.includes('hr-info') || modules.includes('development-info') ? <div className='help-module-section'>
                         <h4 className='help-module-title'>Contact Information</h4>
-                        <div className='help-module-page'>
+                        {modules.includes('hr-info') ? <div className='help-module-page'>
                             <img src={PageIcon} className='help-page-svg' />
                             <h4 className='help-module-link' onClick={() => history.push(`/helpPage?module=hr-info`)}>HR References</h4>
-                        </div>
-                        <div className='help-module-page'>
+                        </div> : ''}
+                        {modules.includes('development-info') ? <div className='help-module-page'>
                             <img src={PageIcon} className='help-page-svg' />
                             <h4 className='help-module-link' onClick={() => history.push(`/helpPage?module=development-info`)}>App Development</h4>
-                        </div>
-                    </div>
+                        </div> : ''}
+                    </div> : ''}
                 </div>
             </div>
         </div>
